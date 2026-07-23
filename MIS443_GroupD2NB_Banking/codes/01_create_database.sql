dbd110de66574ea427ee9f6e31b4d3448fa17f2b
Duong Vu <dduongvu25@gmail.com>
2026-07-23 16:03:29 +0700

    Delete MIS443_GroupD2NB_Banking/codes directory

--- MIS443_GroupD2NB_Banking/codes/01_create_database.sql
+++
@@ -1,6 +0,0 @@
--- MIS443 - Assignment 2: PostgreSQL Database Development and SQL Practice
--- Group: D2NB | Schema: Banking (SQL Practice Online)
--- File 01: Create Database
-
-CREATE DATABASE banking
-
